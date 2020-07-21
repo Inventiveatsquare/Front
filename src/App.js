03cd9b8f717a8cd9e5082056f8b373aab7a47178
Hardeep Singh <expodevclub@gmail.com>
2020-07-21 15:40:26 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,15 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react'
+import axios from 'axios';
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       Welcome to react this is new update
-      </header>
-    </div>
-  );
+
+class App extends Component {
+ 
+  state = {
+      clients: []
+    }
+
+    
+   componentDidMount(){
+
+       const clients =  axios.get('http://www.tryswifty.com/api/person')
+       
+      .then( response => {
+        
+          this.setState({clients: response.data})
+          console.log(response);
+        })
+        // handle success
+      }
+
+     
+     
+
+  render()
+  {
+
+      const clients = this.state.clients.map(client => {
+          return (
+
+  <tr key={client._id}>
+    <th scope="row">{client._id}</th>
+    <td>{client.firstname}</td>
+    <td>{client.lastname}</td>
+  </tr>
+
+    
+          )
+        })
+
+     
+
+
+      return (
+       
+          <div className="container">
+           
+          <div className="row">
+      
+            <div className="col-md-9 mt-9 mx-auto spacingpass1">
+
+              
+              
+
+           <h2>Registered Clients List</h2>
+
+           
+           <table className="table">
+<thead>
+  <tr>
+    <th scope="col">Client ID</th>
+    <th scope="col">Username</th>
+    <th scope="col">Email</th>
+  </tr>
+</thead>
+<tbody>
+{clients}
+
+</tbody>
+</table>
+
+          
+            </div>
+      </div>
+  </div>  
+      )
+  }
+
 }
+
 
 export default App;
